47ceb5a0c79c7eb8cdd4a7c61b2074c7ad503d4c
jordanaluft <jordana.sluft@gmail.com>
2022-07-20 17:39:58 +0100

    Created style header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import logo from '../image/logo.png'
 
 function Header(){
     return (
-       <div>
-            <h1>Hello</h1>
+       <div className='header'>
+            <img className='header--logo' src={logo}/>
+            <h1>ToDo List</h1>
         </div>
     )
 }
