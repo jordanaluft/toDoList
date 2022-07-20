bf698cc219b372c65dba31efc691c2a20c016a25
jordanaluft <jordana.sluft@gmail.com>
2022-07-20 16:19:04 +0100

    changed admin and models to style models view on admin

--- backend/todo/admin.py
+++ backend/todo/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Todo
 
-class TodoAdmin (admin.ModelAdmin):
-    display_list = ('title', 'description', 'completed')
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'completed')
 
 admin.site.register(Todo, TodoAdmin)
