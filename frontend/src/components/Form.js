8ce57faaf57adc384e906b27d976dd54a4b71689
jordanaluft <jordana.sluft@gmail.com>
2022-07-20 18:21:50 +0100

    created and styled form

---
+++ frontend/src/components/Form.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function Form(){
+    return (
+        <div className='form'>
+            <input 
+                type="text"
+                placeholder='Title'
+            />
+            <textArea 
+                type="text"
+                placeholder='Description'
+            />
+            <div className='form--completed'>
+                <input className='form--checkbox' type="checkbox"/>
+                <h3>Completed </h3>
+            </div>
+            <button>
+                Create new item
+            </button>
+        </div>
+        )
+}
+
+export default Form;
